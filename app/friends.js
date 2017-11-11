b9fb53651f131d3d3fbbefaf9442877b868c5c86
ELiud <e.arbelo2@gmail.com>
2017-11-10 21:11:55 -0500

    logic and bootstrap

---
+++ app/friends.js
@@ -0,0 +1,63 @@
+var friends = [ {
+    "name" : "Michael Scott",
+    "photo" : "michael Photo Here",
+    "scores" : [4,5,1,5,5]
+}, {
+    "name" : "Jim Halpert",
+    "photo" : "Jim Photo Here",
+    "scores" : [5,2,5,2,5]
+}, {
+    "name" : "Pam Beasley",
+    "photo" : "Pam Photo Here",
+    "scores" : [5,1,3,5,5]
+}, {
+    "name" : "Dwight Schrute",
+    "photo" : "Dwight Photo Here",
+    "scores" : [5,5,5,5,5]
+}, {
+    "name" : "Andy Bernard",
+    "photo" : "Andy Photo Here",
+    "scores" : [4,1,5,1,5]
+}, {
+    "name" : "Angela Martin",
+    "photo" : "Angela Photo Here",
+    "scores" : [1,5,1,5,5]
+}, {
+    "name" : "Meredeth Palmer",
+    "photo" : "Meredeth Photo Here",
+    "scores" : [2,5,5,5,1]
+}, {
+    "name" : "Kelly Kapoor",
+    "photo" : "Kelly Photo here",
+    "scores" : [3,5,1,5,3]
+}, {
+    "name" : "Ryan Howard",
+    "photo" : "Ryan Photo Here",
+    "scores" : [2,5,1,3,5]
+}, {
+    "name" : "Darryl Philbin",
+    "photo" : "Darryl Photo Here",
+    "scores" : [2,3,5,3,5]
+}, {
+    "name" : "Stanley Hudson",
+    "photo" : "Stanley Photo Here",
+    "scores" : [1,1,1,5,1]
+}, {
+    "name" : "Phyllis Vance",
+    "photo" : "Phyllis Photo Here",
+    "scores" : [2,5,1,5,1]
+}, {
+    "name" : "Toby Flenderson",
+    "photo" : "Toby Photo Here",
+    "scores" : [1,1,5,5,2]
+}, {
+    "name" : "Erin Hannon",
+    "photo" : "Erin Photo Here",
+    "scores" : [5,1,5,5,5]
+}, {
+    "name" : "Oscar Martinez",
+    "photo" : "Oscar Photo Here",
+    "scores" : [1,1,5,5,1]
+}];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies--------------------------------------------------------
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
+// Data file REPLACE WITH MYSQL DATABASE ONCE IT'S COMPLETE------------
+var friends = require('./app/friends.js');
+
+// Body Parser & Express setup-----------------------------------------
+var app = express();
+var port = 3000;
+
+app.use(express.static( '/app/public' ));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// Routes--------------------------------------------------------------
+require('./app/apiRoutes.js')(app);
+require('./app/htmlRoutes.js')(app);
+
+// Listen--------------------------------------------------------------
+app.listen(port);
+console.log("You are connected to port: " + port);
